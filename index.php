2b8610271f9bbba9773253c80cfd0d1eb0467cc8
addinall <addinall@addinall.net>
2015-07-01 07:18:48 +1000

    Morning structure implentation

--- index.php
+++ index.php
@@ -25,21 +25,41 @@
 
 ?>
 
-         
-             
-
-
 <head>
-    <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-    <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
     <!--[if IE]>
         <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <![endif]-->
 </head>
 
 <body>
-<h1>Career Solutions</h1>
+<h1>Career Solutions - Development Testing</h1>
 <hr>
+
+<p>
+This project is a re-engineering of an existing native Windows application. <br>
+Taking a stand alone .EXE system and presenting it as a Web2.x application. The technologies include:
+</p>
+
+<ul>
+    <li>HTML5</li>
+    <li>CSS3/Sass</li>
+    <li>AngularJS</li>
+    <li>Slim</li>
+    <li>NotORM</li>
+    <li>Bootstrap</li>
+    <li>Git (obviously)</li>
+    <li>Composer</li>
+    <li>Bower</li>
+    <li>PHP 5.x</li>
+    <li>jQuery</li>
+</ul>
+<p>
+It will be implemented as a RESPONSIVE/FLUID site that will be at home on fondleslabs.<br> 
+The development stack will operate on a Linux/UNIX or Windows machine. We will use an agile<br> 
+ methodology. The KISS principle is being implemented here!
+</p>
 </body>
 </html>
 
