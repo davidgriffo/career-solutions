bc5bb2457df5bf7e646024341bc8f33d211b8e3d
addinall <addinall@addinall.net>
2015-06-30 16:04:13 +1000

    Setup development structure and environment

---
+++ index.php
@@ -0,0 +1,47 @@
+<DOCTYPE html>
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// CAPTAIN SLOG
+// File         : index.php
+// System       : Career Solutions 
+// Date         : June 30 2015
+// Author       : Mark Addinall
+// Synopsis     : This project is a re-engineering of an existing native Windows application. 
+//                Taking a stand alone .EXE system and presenting it as a Web2.x application. The technologies include
+//                  HTML5
+//                  CSS3/Sass
+//                  AngularJS
+//                  Slim
+//                  NotORM
+//                  Bootstrap
+//                  Git (obviously)
+//                  Composer
+//                  Bower
+//                  PHP 5.x
+//                  jQuery
+//                It will be implemented as a RESPONSIVE/FLUID site that will be at home on fondleslabs. 
+//                The development stack will operate on a Linux/UNIX or Windows machine. We will use an agile 
+//                methodology. The KISS principle is being implemented here!
+
+?>
+
+         
+             
+
+
+<head>
+    <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+        <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+</head>
+
+<body>
+<h1>Career Solutions</h1>
+<hr>
+</body>
+</html>
+
+             
+
